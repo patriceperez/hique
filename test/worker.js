41e83dd4dd765b403d3b961ef01ae9ef2d0d0ef8
patriceperez <patrice.perez@kenshoo.com>
2016-07-03 18:30:15 +0300

    - fixing bug with job which have no data - added ability to inject any redis object (not guarantying compatibility with all libraries)  - changed status of created jobs from 'active' to 'pending' for cbetter larity

--- test/worker.js
+++ test/worker.js
@@ -6,27 +6,64 @@ var defaultConfig = require('../config/defaults');
 var Worker = require('../lib/worker');
 
 describe('Worker', function () {
-    it('should construct default worker', function () {
-        var testWorker = new Worker();
+    var testWorker;
 
+    beforeEach(function () {
+        testWorker = new Worker({});
+    });
+
+    it('should construct default worker', function () {
         assert.isNotNull(testWorker, 'worker object is NULL');
     });
 
     it('should construct default worker monitor', function () {
-        var testWorker = new Worker();
-
         assert.isNotNull(testWorker.monitor, 'worker monitor object is NULL');
     });
 
     it('should construct default worker job', function () {
-        var testWorker = new Worker();
-
         assert.isNotNull(testWorker, 'worker job object is NULL');
     });
 
     it('should contain default config', function () {
-        var testWorker = new Worker();
-
         assert.equal(testWorker.config, defaultConfig, 'config object does not equal to defaults');
+    });
+
+    it('be active by default', function () {
+        assert.isTrue(testWorker.active, 'active flag should be true');
+    });
+
+    it('be pausable', function () {
+        testWorker.pause();
+        assert.isNotTrue(testWorker.active, 'active flag should be false after pausing');
+    });
+
+    it('be able to resume from paused state', function () {
+        testWorker.pause();
+        testWorker.start();
+
+        assert.isTrue(testWorker.active, 'active flag should be true after resuming work');
+    });
+
+    it('should create a job with no data', function (done) {
+        testWorker.createJob('testJob').save(function (err, job) {
+            if (err) throw err;
+            assert.isNotNull(job.data, 'data object should not be null when no data was supplied');
+            done();
+        });
+    });
+
+    it('should get an existing job object', function (done) {
+        testWorker.createJob('testJob').save(function (err, job) {
+            if (err) throw err;
+
+            testWorker.getJob('testJob', job.id, function (err, existingJob) {
+                if (err) throw err;
+
+                assert.equal(job.id, existingJob.id, 'existing job id should be equal to the one created');
+                assert.equal(job.data, existingJob.data, 'existing job data should be equal to the one created');
+                assert.equal(job.status, existingJob.status, 'existing job status should be equal to the one created');
+                done();
+            });
+        });
     });
 });
